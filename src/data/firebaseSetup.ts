d812ba143675e469977d3f45bc7acfa6dfeeefa5
Disky Aria Jetmiko <disky.jetmiko@gmail.com>
2023-04-27 15:57:44 +0700

    add firebase to the project

---
+++ src/data/firebaseSetup.ts
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCm1iZIPjASPZh9AXi1D-8qoSORqfEFixM",
+  authDomain: "doggoneedle.firebaseapp.com",
+  projectId: "doggoneedle",
+  storageBucket: "doggoneedle.appspot.com",
+  messagingSenderId: "66980404913",
+  appId: "1:66980404913:web:be52680d1955b57fc94c90",
+  measurementId: "G-1EY9J5GS0S"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const analytics = getAnalytics(app);
